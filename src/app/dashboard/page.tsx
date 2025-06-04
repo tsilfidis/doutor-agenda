420fcdc4f27c43f7af384496a30b27b09c381209
Panagitis Alexandro Tsilfidis <tsilfidis@gmail.com>
2025-06-04 11:13:15 -0400

    feat: add clinic form page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,9 @@
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import SignOutButton from "./components/sign-uot-button";
@@ -12,6 +15,13 @@ const DashboardPage = async () => {
 
   if (!session?.user) {
     redirect("/authentication");
+  }
+
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+  if (clinics.length === 0) {
+    redirect("/clinic-form");
   }
 
   return (
