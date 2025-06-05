1c61afd56bbb034989b755772059e2b58c1a195f
Panagitis Alexandro Tsilfidis <tsilfidis@gmail.com>
2025-06-05 16:08:38 -0400

    fix: ensure doctor form is cleared when close

--- src/app/(protected)/doctors/_components/upsert-doctor-form.tsx
+++ src/app/(protected)/doctors/_components/upsert-doctor-form.tsx
@@ -75,6 +75,7 @@ interface UpsertDoctorFormProps {
 
 const UpsertDoctorForm = ({ doctor, onSuccess }: UpsertDoctorFormProps) => {
   const form = useForm<z.infer<typeof formSchema>>({
+    shouldUnregister: true,
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: doctor?.name || "",
