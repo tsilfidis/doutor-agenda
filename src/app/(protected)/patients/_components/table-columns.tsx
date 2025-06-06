8d37b49a07031d8c2fa1d1b436fe75b70858206c
Panagitis Alexandro Tsilfidis <tsilfidis@gmail.com>
2025-06-06 11:11:16 -0400

    feat: ensure patient can be update and delete

--- src/app/(protected)/patients/_components/table-columns.tsx
+++ src/app/(protected)/patients/_components/table-columns.tsx
@@ -3,9 +3,25 @@ import { ColumnDef } from "@tanstack/react-table";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import { EditIcon, MoreVerticalIcon, TrashIcon } from "lucide-react";
+import { useAction } from "next-safe-action/hooks";
+import { useState } from "react";
 import { PatternFormat } from "react-number-format";
+import { toast } from "sonner";
 
+import { deletePatient } from "@/actions/delete-patient";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -17,10 +33,96 @@ import {
 import { patientsTable } from "@/db/schema";
 
 import { formatSex } from "../_helpers/format-sex";
+import UpsertPatientForm from "./upsert-patient-form";
 
 dayjs.extend(utc);
 
 type Patient = typeof patientsTable.$inferSelect;
+
+interface TableActionsProps {
+  patient: Patient;
+}
+
+const TableActions = ({ patient }: TableActionsProps) => {
+  const [isUpsertPatientDialogOpen, setIsUpsertPatientDialogOpen] =
+    useState(false);
+
+  const deletePatientAction = useAction(deletePatient, {
+    onSuccess: () => {
+      toast.success("Paciente deletado com sucesso.");
+    },
+    onError: () => {
+      toast.error("Erro ao deletar paciente.");
+    },
+  });
+
+  const handleDeletePatientClick = () => {
+    deletePatientAction.execute({ id: patient.id });
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button variant="ghost" size="icon">
+          <MoreVerticalIcon />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>
+          {patient.name
+            .split(" ")
+            .filter((_, i, arr) => i === 0 || i === arr.length - 1)
+            .join(" ")}
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <Dialog
+          open={isUpsertPatientDialogOpen}
+          onOpenChange={setIsUpsertPatientDialogOpen}
+        >
+          <DialogTrigger asChild>
+            <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+              <EditIcon className="mr-2 h-4 w-4" />
+              Editar
+            </DropdownMenuItem>
+          </DialogTrigger>
+          <UpsertPatientForm
+            isOpen={isUpsertPatientDialogOpen}
+            defaultValues={patient}
+            onSuccess={() => setIsUpsertPatientDialogOpen(false)}
+          />
+        </Dialog>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <DropdownMenuItem
+              onSelect={(e) => e.preventDefault()}
+              className="text-destructive"
+            >
+              <TrashIcon className="text-destructive mr-2 h-4 w-4" />
+              Deletar
+            </DropdownMenuItem>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>
+                Tem certeza que deseja deletar esse paciente?
+              </AlertDialogTitle>
+              <AlertDialogDescription>
+                Essa ação não pode ser revertida. Isso irá deletar o paciente e
+                todas as consultas agendadas.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancelar</AlertDialogCancel>
+              <AlertDialogAction onClick={handleDeletePatientClick}>
+                Deletar
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 export const PatientTableColumns: ColumnDef<Patient>[] = [
   {
@@ -66,32 +168,7 @@ export const PatientTableColumns: ColumnDef<Patient>[] = [
   {
     id: "actions",
     cell: ({ row }) => {
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <Button variant="ghost" size="icon">
-              <MoreVerticalIcon />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>
-              {row.original.name
-                .split(" ")
-                .filter((_, i, arr) => i === 0 || i === arr.length - 1)
-                .join(" ")}
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <EditIcon />
-              Editar
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <TrashIcon />
-              Excluir
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
+      return <TableActions patient={row.original} />;
     },
   },
 ];
