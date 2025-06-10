fe6d4ceb31aef5df6d18b2cacaf071769dea5066
Panagiotis Alexandro Tsilfidis <105652220+tsilfidis@users.noreply.github.com>
2025-06-09 20:28:33 -0400

    Atualizar o page.tsx

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -55,7 +55,7 @@ const DoctorsPage = async () => {
             </p>
           </div>
         ) : (
-          <div className="grid grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
             {doctors.map((doctor) => (
               <DoctorCard key={doctor.id} doctor={doctor} />
             ))}
